7eb4ab96b0b126572787dd6adebbfc80a16bb70b
Rostyslav Zabolotnyi <rstzab@dsg.tuwien.ac.at>
2016-11-15 16:00:28 +0200

    some comments and changes.

--- src/IO/WorkFile.java
+++ src/IO/WorkFile.java
@@ -6,19 +6,22 @@ import java.util.Map;
 
 /**
  * forRST: general. Should I create separate class for methods like "replacer", "fileContentReplacer" below
+ * forMHS: it depends. I think it's fine as it is.
  */
 public class WorkFile {
 
     private File inputFile;
     private File outputFile;
 
+    
+    // forMHS: Reuse constructors as much as possible. This allows you to put general initialization logic to the single place.
+    
     /**
      * Assign one file for input and output
      * @param file
      */
     public WorkFile(File file){
-        inputFile = file;
-        outputFile = file;
+        this(file, file);
     }
 
     /**
@@ -29,6 +32,7 @@ public class WorkFile {
     public WorkFile(File inputFile, File outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+        // forMHS: this is the main constructor and all common logic is here.
     }
 
     /**
@@ -36,12 +40,9 @@ public class WorkFile {
      * @param path
      */
     public WorkFile(String path){
-        inputFile = Paths
-                .get(path)
-                .toFile();
-        outputFile = Paths
-                .get(path)
-                .toFile();
+        // forMHS: why didn't you use https://docs.oracle.com/javase/7/docs/api/java/io/File.html#File(java.lang.String) constructor?
+        // forMHS: left your code for now.
+        this(Paths.get(path).toFile());
     }
 
     /**
@@ -50,12 +51,7 @@ public class WorkFile {
      * @param outPath output file path
      */
     public WorkFile(String inPath, String outPath){
-        inputFile = Paths
-                .get(inPath)
-                .toFile();
-        outputFile = Paths
-                .get(outPath)
-                .toFile();
+        this(Paths.get(inPath).toFile(), Paths.get(outPath).toFile());
     }
 
 
@@ -67,9 +63,15 @@ public class WorkFile {
     public String readFile() throws IOException {
         /*
         forRST: is there a better class to read from file string text?
-        How can I read cyrillic text
+        forMHS: http://www.adam-bien.com/roller/abien/entry/java_8_reading_a_file
+        forRST: How can I read cyrillic text?
+        forMHS: http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#String(byte[],%20java.nio.charset.Charset)    
          */
 
+         // forMHS: Never EVER build string like this! Count how many string allocations you're doing! That's awful!
+         // forMHS: use https://docs.oracle.com/javase/7/docs/api/java/lang/StringBuilder.html
+         // forMHS: http://stackoverflow.com/questions/5234147/why-stringbuilder-when-there-is-string/5234160#5234160
+         // forMHS: http://stackoverflow.com/questions/18453458/string-builder-vs-string-concatenation/18453485#18453485
         String buffer = "";
         try(FileInputStream in = new FileInputStream(inputFile)){
             int b;
@@ -88,6 +90,7 @@ public class WorkFile {
     public void writeFile(String text) throws IOException{
         /*
         forRST: is there a better class to write string text to file ?
+        forMHS: http://www.adam-bien.com/roller/abien/entry/java_7_writing_a_string
         */
         try(PrintWriter out = new PrintWriter ( outputFile )){
             out.print(text);
@@ -102,6 +105,7 @@ public class WorkFile {
 
         /*
         forRST: is there a better class to write string text to file ?
+        forMHS: http://www.adam-bien.com/roller/abien/entry/java_7_writing_a_string
         */
 
         try(PrintWriter out = new PrintWriter ( new FileOutputStream(outputFile, true) )){
@@ -117,8 +121,14 @@ public class WorkFile {
     public String replacer(Map<String, String> map) throws IOException{
         String result = null;
 
+        //forMHS: btw, you'd better use 
+        // https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#replace-java.lang.CharSequence-java.lang.CharSequence-
+        // or https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#replaceAll-java.lang.String-java.lang.String-
+        // instead of your implementation.
+        
         // receive distinct words from inputFile
         //forRST: is there a possibility to make more complex split options (split by space AND by paragraph AND by coma) ?
+        //forMHS: http://docs.oracle.com/javase/7/docs/api/java/lang/String.html#split(java.lang.String) splits by any Regular Expression.
         String[] words = readFile()
                 .split(" ");
 
@@ -158,6 +168,8 @@ public class WorkFile {
         Such an argument may be confusing as it is unclear if a varargs or non-varargs call is desired. For example System.out.printf("%s", null).
 
          */
+         // forMHS: https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#join-java.lang.CharSequence-java.lang.CharSequence...-
+         // forMHS: this is a *static* method and should be referenced as "String.join(" ", words)". That's one of Java design faults, but that's what we have.
         return result.join(" ",words);
     }
 
@@ -200,6 +212,7 @@ public class WorkFile {
         String[] words = readFile()
                 .split(" ");
 
+                // forMHS: I don't like this implementation, but I haven't got it yet.
         //search for the word
         for (String word : words) {
             String pureWord = word.replaceAll("[^A-Za-z]+", "");
